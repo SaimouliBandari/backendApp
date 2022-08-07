7563b14c1ddfe8b5a3c3cb6ba4a06d4690698876
SaimouliBandari <saimouli4k@gmail.com>
2022-08-07 15:59:56 +0530

    Sreve an Html File

--- myApp.js
+++ myApp.js
@@ -2,8 +2,7 @@ let express = require('express');
 let app = express();
 console.log('Hello World');
     app.get('/', (req, res) =>{
-        res.sendFile(__dirname + '/views/index.html');
-        res.send('Hello Express');
+        res.sendFile(__dirname + "/views/index.html");
     });
 
 
