0ebbf01842e6e4d46ab3a8fc48f58f42f5cd28c1
SaimouliBandari <saimouli4k@gmail.com>
2022-08-07 21:48:52 +0530

    middleware

--- myApp.js
+++ myApp.js
@@ -6,18 +6,20 @@ console.log('Hello World');
     app.use("/public", express.static(__dirname + "/public"));
     app.get('/', (req, res) =>{
         res.sendFile(__dirname + "/views/index.html");
-        
     });
 
     app.get('/json' , (req, res) =>{
         let mess = "Hello json";
         if(process.env.MESSAGE_STYLE == vcase){
             mess = mess.toUpperCase();
-            
         }
-
         res.json({message : mess});
-        
+    });
+
+    app.use((req, res, next) => {
+        var str = req.method + " " + req.path + "-" + req.ip;
+        console.log(str);
+        next();
     });
         
 
